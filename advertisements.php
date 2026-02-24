144f437e3a8b0cc2d7be2f5ef5c8a5dbc8c1ce6a
Rodrick <other@deadinsideartist.art>
2026-02-24 02:08:32 -0600

    fixed url

--- advertisements.php
+++ advertisements.php
@@ -22,7 +22,7 @@
                     foreach ($ads as $key => $value) {
                         echo "<li>
                         <p><a target='_blank' href='{$value[2]}'>{$value[0]}</a> by {$value[1]}</p>
-                        <img src='{$value[3]}' alt='{$value[0]} Ad'>
+                        <img src='ads/{$value[3]}' alt='{$value[0]} Ad'>
                         </li>";
                     }
                 ?>
